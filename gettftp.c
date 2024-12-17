c755b81cff00a725c09c1a3cbe73da44f2699330
mael-lukas <mael.lukas@ensea.fr>
2024-12-17 16:17:33 +0100

    q4.a) done

--- gettftp.c
+++ gettftp.c
@@ -5,8 +5,12 @@
 #include <string.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define MAXSIZE 256
+#define RRQ 1
+#define WRQ 2
+#define SERVER_PORT 1069
 
 int main(int argc, char** argv) {
     // check number of arguments
@@ -15,6 +19,7 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
+    char* localhost = "127.0.0.1";
     char* host = argv[1];
     char* file = argv[2];
 
@@ -38,11 +43,31 @@ int main(int argc, char** argv) {
 
     char sendBuffer[MAXSIZE] = {0};
     char receiveBuffer[MAXSIZE] = {0};
+    // filling the send buffer
+    // first two bytes used for operation code, here read request
+    sendBuffer[0] = 0;
+    sendBuffer[1] = RRQ;
+    // next bytes are filled with the file name we want to read
+    sprintf(sendBuffer + 2,"%s",file);
+    // reserved byte
+    sendBuffer[2+strlen(file)] = 0;
+    // ascii mode
+    sprintf(sendBuffer + 3 + strlen(file), "NETASCII");
+    // reserved byte
+    sendBuffer[11 + strlen(file)] = 0;
 
-    struct sockaddr_in sockadd;
-    sockadd.sin_family = res->ai_family;
-    sockadd.sin_port = htons(1069);
+    int sendBufferSize = 12 + strlen(file);
 
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = res->ai_family;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET,localhost,&serverAddr.sin_addr.s_addr);
+
+    ssize_t sentBytes = sendto(sfd,sendBuffer,sendBufferSize,0,&serverAddr,sizeof(serverAddr));
+    if (sentBytes == -1 ) {
+        printf("Error while sending request\r\n");
+        exit(EXIT_FAILURE);
+    }
 
 
     return 0;
