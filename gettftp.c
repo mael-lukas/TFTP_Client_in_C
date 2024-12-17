37bb0d2932e6414adb4e9964535da16f1f5835ef
mael-lukas <mael.lukas@ensea.fr>
2024-12-17 15:14:26 +0100

    q3 done

--- gettftp.c
+++ gettftp.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <netdb.h>
+#include <netinet/in.h>
+
+#define MAXSIZE 256
 
 int main(int argc, char** argv) {
     // check number of arguments
     if (argc != 3) {
-        printf("Invalid number of arguments: expected 2, received %d\r\n",argc);
+        printf("Invalid number of arguments: expected 3, received %d\r\n",argc);
+        exit(EXIT_FAILURE);
     }
 
     char* host = argv[1];
@@ -21,8 +26,23 @@ int main(int argc, char** argv) {
 
     int status = getaddrinfo(host, NULL, &hints, &res);
     if (status == -1) {
-        printf("Unable to reach host %s\r\n",host);
+        printf("Unable to reach host: %s\r\n",host);
+        exit(EXIT_FAILURE);
     }
+    int sfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+    if (sfd == -1) {
+        perror("socket error");
+        exit(EXIT_FAILURE);
+    }
+    printf("created socket\r\n");
+
+    char sendBuffer[MAXSIZE] = {0};
+    char receiveBuffer[MAXSIZE] = {0};
+
+    struct sockaddr_in sockadd;
+    sockadd.sin_family = res->ai_family;
+    sockadd.sin_port = htons(1069);
+
 
 
     return 0;
